6a15ba86d4e2ff2c8b77daa01fe3180244409e9c
Анька <128055222+crazybutproud@users.noreply.github.com>
2024-02-25 03:08:34 +0300

    Add files via upload

--- TestSpring.java
+++ TestSpring.java
@@ -9,13 +9,16 @@ public class TestSpring {
         );
         //Music beanMusic = classPathXmlApplicationContext.getBean("musicBean",Music.class);
         //MusicPlayer musicPlayer = new MusicPlayer(beanMusic); //внедряем зависимость вручную с помощью конструктора
+//
+//        MusicPlayer musicPlayer = classPathXmlApplicationContext.getBean("musicPlayerBean",MusicPlayer.class); //метод с dependencies injection
+//
+//        musicPlayer.playMusic(); //использует зависимость чтобы реализовать метод
+//
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
 
-        MusicPlayer musicPlayer = classPathXmlApplicationContext.getBean("musicPlayerBean",MusicPlayer.class); //метод с dependencies injection
-
-        musicPlayer.playMusic(); //использует зависимость чтобы реализовать метод
-
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        RockMusic rockMusic = classPathXmlApplicationContext.getBean("musicBean",RockMusic.class);
+        System.out.println(rockMusic.getSong());
 
         classPathXmlApplicationContext.close();
     }
